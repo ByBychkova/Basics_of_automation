747627da0f8d1cc8e476384bec8f65378d9b0bfc
Bychkova Mariya <bychk-mariya@yandex.ru>
2023-10-30 19:24:35 +0300

    TestClass

---
+++ src/test/java/ru/netology/javaqa/CashBackHackerTestJunit4.java
@@ -0,0 +1,53 @@
+package ru.netology.javaqa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CashBackHackerTestJunit4 {
+    @Test
+    public void boundaryValue1() {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    @ Test
+    public void boundaryValue2() {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 1;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void boundaryValue3() {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void boundaryValue4() {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test() {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 500;
+        int actual = service.remain(amount);
+        int expected = 500;
+        assertEquals(expected, actual);
+    }
+}
